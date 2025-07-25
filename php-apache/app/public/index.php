2610ab8be3d31447d413a5b1a9bd8b76e0b4fca2
米本 朋未 <yonemototomomi@yonemotonoMacBook-Air.local>
2025-07-25 10:44:09 +0900

    不要コメントを削除

--- php-apache/app/public/index.php
+++ php-apache/app/public/index.php
@@ -5,11 +5,3 @@ try {
 } catch (PDOException $e) {
     echo '<h1 style="color:red">Failed<br><p>' . $e->getMessage() . '</p>';
 }
-
-// <?php
-// try {
-//     $dbh = new PDO('mysql:dbname=[ 6 ];host=[ 7 ];unix_socket=/tmp/mysql.sock', 'root', '[ 8 ]');
-//     echo '<h1 style="color:yellow">Congratulations</h1>';
-// } catch (PDOException $e) {
-//     echo '<h1 style="color:red">Failed<br><p>' . $e->getMessage() . '</p>';
-// }
